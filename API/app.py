209904771594ca0e8d010a1c9122e268e8f7a80a
Narayanan Asuri Krishnan <narayananak12@gmail.com>
2022-01-09 15:18:53 -0500

    Second commit

--- API/app.py
+++ API/app.py
@@ -24,4 +24,4 @@ api.add_resource(Create, "/create")
 api.add_resource(Display, "/display")
 
 if __name__ == "__main__":
-    app.run(debug = True, port=5000)
+    app.run(debug = True,host = "127.0.0.1", port=5000)
--- UI/app.py
+++ UI/app.py
@@ -30,5 +30,5 @@ def display():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True,host = "127.0.0.1",port=5001)
 
