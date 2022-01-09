20b0e4551806e08b0fb72d9f8b805e269b3b0377
Narayanan Asuri Krishnan <narayananak12@gmail.com>
2022-01-09 15:48:38 -0500

    Update app.py

--- API/app.py
+++ API/app.py
@@ -5,8 +5,10 @@ from Client import Client
 app = Flask(__name__)
 api = Api(app)
 
+#Connect to Neo4J
 neo = Client("bolt://127.0.0.1:7687", "neo4j", "neo")
 
+#API to create employee
 class Create(Resource):
     def post(self):
         data = request.form
@@ -14,6 +16,7 @@ class Create(Resource):
         neo.createEmployee(data["emp_name"], data["emp_id"])
         return data
 
+#API to display all employees
 class Display(Resource):
     def get(self):
         data = neo.returnEmployees()
